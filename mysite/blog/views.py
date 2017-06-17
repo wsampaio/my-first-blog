f0419e4159b7febf73d648975cff7726b755c819
admin <admin@me.com>
2017-06-17 15:30:55 -0300

    criado template base

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 
